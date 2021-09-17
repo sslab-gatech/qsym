8f7e5ed418480d5252f2db29dcc62293b34ba441
xdkong <36298629+bjchan9an@users.noreply.github.com>
2021-09-17 16:39:46 +0800

    Install pip with get-pip.py instead of apt-get
    We failed to build the docker since the pip version may be too old when installed with apt-get (e.g., 8.1, in ubuntu 16.04).
    
    To fix this issue, we modify the ``setup.sh`` where we install pip with ``https://bootstrap.pypa.io/get-pip.py`` instead of ``apt-get install python-pip``. This works well for me.

--- setup.sh
+++ setup.sh
@@ -12,8 +12,14 @@ git submodule update
 # install system deps
 sudo apt-get update
 sudo apt-get install -y libc6 libstdc++6 linux-libc-dev gcc-multilib \
-  llvm-dev g++ g++-multilib python python-pip \
+  llvm-dev g++ g++-multilib python \
   lsb-release
+
+# install pip
+sudo apt-get install wget
+wget https://bootstrap.pypa.io/get-pip.py
+python get-pip.py
+rm -rf ./get-pip.py
 
 # install z3
 pushd third_party/z3
